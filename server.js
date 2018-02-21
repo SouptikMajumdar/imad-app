4d1fe90dc169027369766740184f8257065f17ce
SouptikMajumdar <32878956+SouptikMajumdar@users.noreply.github.com>
2018-02-21 21:08:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,13 +98,18 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-/*app.get('/article-three', function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-three.html'));
-});*/
 app.get('/:articleName', function(req, res) {
     var articleName = req.pararms.articleName; //to extract ...express framework
     res.send(createTemplate(articles[articleName]));
 });
+
+
+
+
+/*app.get('/article-three', function(req,res){
+    res.sendFile(path.join(__dirname,'ui','article-three.html'));
+});*/
+
 
 /*app.get('/article-one', function(req,res){
     res.sendFile(path.join(__dirname,'ui','article-one.html'));
